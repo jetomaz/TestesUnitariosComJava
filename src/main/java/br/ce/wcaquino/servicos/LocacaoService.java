40395dc08c715bac2d4bb64a7f77e67716a6a34a
Eduardo <jetomaz@gmail.com>
2022-11-10 00:58:04 -0300

    incluindo jUnit nos testes

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -28,7 +31,20 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste(){
+		//cenario - Inicializamos os objetos e variaveis necessarios para criar o cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Eduardo");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		//acao - realizamos a acao - funcionalidade que queremos testar
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificação - verificamos os resultados retornados
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		
+		
 		
 	}
 }
